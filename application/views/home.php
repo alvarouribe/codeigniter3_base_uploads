0cbeb9c3194b014d05b0caddeb87e4671a051062
Alvaro Uribe <iam@alvarouribe.cl>
2020-09-12 15:19:18 +1200

    base project to upload pictures

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row">
+		<div class="col-12 col-lg-6">
+			<div class="p-4 border">
+				<h1 class="mt-3">Encuentra tu curso</h1>
+				<p>Es rápido y fácil.</p>
+			</div>
+		</div>
+	</div>
+</div>
